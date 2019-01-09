9464fa73b9724a75ca7839ac45894f2207858ceb
zhhjoseph <zhhjoseph@gmail.com>
2019-01-09 15:29:31 -0500

    done basic switch routes for app.js

--- client/app.js
+++ client/app.js
@@ -2,12 +2,23 @@ import React from 'react'
 import '../../public/style.css'
 import {Navbar} from './components'
 import Routes from './routes'
+import {Navlink, Switch, Route, withRouter} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <main>
+        <Switch>
+          <Route path="/products" component={Allgames} />
+          {/* <Route path = '/products/xbox' component = {Xbox}/> */}
+          {/* <Route path = '/products/pc' component = {Pc}/> */}
+          {/* <Route path = '/products/playstation' component = {Playstation}/> */}
+          {/* <Route path = '/products/switch' component = {Switch}/> */}
+          {/* <Route path = '/products/:id' component = {SingleGame}/> */}
+        </Switch>
+      </main>
     </div>
   )
 }
