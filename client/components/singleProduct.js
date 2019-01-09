c8481e4373ed6b7fd76a42fef68e0cbb49abd42f
Ken Atienza <atienza.ken@gmail.com>
2019-01-09 18:53:14 -0500

    dropdown menu added

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import axios from 'axios'
-import {Image, Item} from 'semantic-ui-react'
+import {Image, Item, Dropdown} from 'semantic-ui-react'
 
 class singleProduct extends Component {
   constructor() {
     super()
     this.state = {
-      game: {}
+      game: {},
+      selected: ''
     }
   }
 
@@ -22,11 +23,22 @@ class singleProduct extends Component {
     })
   }
 
+  onChangeDropdown(event, data) {
+    this.setState({selected: data.value})
+  }
+
   render() {
     const {game} = this.state
     let price = `$ ${game.currentPrice}`
     price =
       price.slice(0, price.length - 2) + '.' + price.slice(price.length - 2)
+
+    const consoles = [
+      {key: 'PC', text: 'PC', value: 'PC'},
+      {key: 'SW', text: 'Switch', value: 'Switch'},
+      {key: 'XB', text: 'Xbox', value: 'Xbox'},
+      {key: 'PS', text: 'Playstation', value: 'Playstation'}
+    ]
 
     return (
       <Item.Group>
@@ -40,6 +52,14 @@ class singleProduct extends Component {
           <Item.Content>
             <Item.Header>{game.name}</Item.Header>
             <Item.Description>{game.description}</Item.Description>
+            <Dropdown
+              placeholder="console"
+              search
+              selection
+              options={consoles}
+              value={this.state.selected}
+              onChange={this.onChangeDropdown.bind(this)}
+            />
             <Item.Meta>
               <span className="price">{price}</span>
             </Item.Meta>
