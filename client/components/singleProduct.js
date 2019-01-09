23e04f60a6db5a7229deae8dbcbabc20cba61a61
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-01-09 15:52:25 -0500

    div setup

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,1 +1,9 @@
 //jsh;kjqrwherkhqw
+import {Component} from 'react'
+import {connect} from 'react-redux'
+
+class singleProduct extends Component {
+  render() {
+    return <div>hello</div>
+  }
+}
