c8b3476d4c406ea807be96f7f5efcf3127013d1b
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-01-09 16:55:13 -0500

    react basics for singlePage ready for semantic

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,9 +1,35 @@
 //jsh;kjqrwherkhqw
-import {Component} from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import axios from 'axios'
 
 class singleProduct extends Component {
+  constructor() {
+    super()
+    this.state = {
+      game: {}
+    }
+  }
+
+  async componentDidMount() {
+    const singleGame = await axios.get(
+      `/api/products/${this.props.match.params.id}`
+    )
+    this.setState(() => {
+      return {
+        game: singleGame.data
+      }
+    })
+  }
+
   render() {
-    return <div>hello</div>
+    const {game} = this.state
+    return (
+      <div>
+        <h1>{game.name}</h1>
+      </div>
+    )
   }
 }
+
+export default singleProduct
