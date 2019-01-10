ce4fc9a0692d47706d079fa28c90e4efa275d02d
zhhjoseph <zhhjoseph@gmail.com>
2019-01-09 19:11:49 -0500

    added basic Navbar with minimal functionality

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,29 +3,35 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {Menu, Image, Dropdown, Icon, Search} from 'semantic-ui-react'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+const Navbar = () => (
+  <Menu inverted color="black">
+    <Menu.Item as={Link} to="/">
+      <Image circular size="tiny" centered src="icon.png" />
+    </Menu.Item>
+    <Menu.Item>
+      <Dropdown text="Platforms" button color="black">
+        <Dropdown.Menu>
+          <Dropdown.Item as={Link} to="/products">
+            All platforms
+          </Dropdown.Item>
+          <Dropdown.Item icon="computer" text="PC" />
+          <Dropdown.Item icon="xbox" text="Xbox" />
+          <Dropdown.Item icon="playstation" text="Playstation" />
+          <Dropdown.Item icon="nintendo switch" text="Switch" />
+        </Dropdown.Menu>
+      </Dropdown>
+    </Menu.Item>
+    <Menu.Item>New Releases</Menu.Item>
+    <Menu.Item>Sale</Menu.Item>
+    <Menu.Item position="right" fitted>
+      <Search />
+    </Menu.Item>
+    <Menu.Item>
+      <Icon name="shopping cart" size="large" />
+    </Menu.Item>
+  </Menu>
 )
 
 /**
