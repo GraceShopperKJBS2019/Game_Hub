dd961ea447ee326fab1feb13a540eb258aac53d6
zhhjoseph <zhhjoseph@gmail.com>
2019-01-09 23:31:30 -0500

    basic all pc page rendered

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import '../public/style.css'
-import {Navbar, AllGames, singleProduct} from './components'
+import {Navbar, AllGames, singleProduct, AllPcGames} from './components'
 import Routes from './routes'
 import {Navlink, Switch, Route, withRouter} from 'react-router-dom'
 
@@ -13,7 +13,7 @@ const App = () => {
         <Switch>
           <Route exact path="/products" component={AllGames} />
           {/* <Route path = '/products/xbox' component = {Xbox}/> */}
-          {/* <Route path = '/products/pc' component = {Pc}/> */}
+          <Route path="/products/pc" component={AllPcGames} />
           {/* <Route path = '/products/playstation' component = {Playstation}/> */}
           {/* <Route path = '/products/switch' component = {Switch}/> */}
           <Route path="/products/:id" component={singleProduct} />
