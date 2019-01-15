245ca0bfe00ec0d2ec8e0dc93facdababea00571
zhhjoseph <zhhjoseph@gmail.com>
2019-01-14 21:42:17 -0500

    basic list of order transactions done

--- client/components/orderhistory.js
+++ client/components/orderhistory.js
@@ -2,34 +2,42 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Item, Segment, Header} from 'semantic-ui-react'
 
+const priceHelper = price => {
+  let ret = `$${price}`
+  return `${ret.slice(0, ret.length - 2) + '.' + ret.slice(ret.length - 2)}`
+}
+
 const orderList = props => {
+  console.log(props)
   const {orderHistory} = props
   return (
-    <Segment>
-      <Header name="Past Transactions" />
-      <Item.Group divided>
-        {orderHistory.map(orders => {
-          console.log('PAST ORDERS --->', orders)
-          return (
-            <Item key={orders.id}>
-              <Item.Image size="tiny" src={orders.imageURL} />
-              <Item.Content verticalAlign="middle">
-                <Item.Header>{orders.productName}</Item.Header>
-                <Item.Meta>
-                  <span>{orders.checkoutPrice}</span>
-                </Item.Meta>
-              </Item.Content>
-            </Item>
-          )
-        })}
-      </Item.Group>
-    </Segment>
+    <div>
+      <Segment>
+        <Header name="Past Transactions" />
+        <Item.Group divided>
+          {orderHistory.map(orders => {
+            console.log('PAST ORDERS --->', orders)
+            return (
+              <Item key={orders.id}>
+                <Item.Image size="tiny" src={orders.imageURL} />
+                <Item.Content verticalAlign="middle">
+                  <Item.Header>{orders.productName}</Item.Header>
+                  <Item.Meta>
+                    <span>{priceHelper(orders.checkoutPrice)}</span>
+                  </Item.Meta>
+                </Item.Content>
+              </Item>
+            )
+          })}
+        </Item.Group>
+      </Segment>
+    </div>
   )
 }
 
 const mapStateToProps = state => {
   return {
-    history: state.orderHistory
+    orderHistory: state.orderHistory
   }
 }
 
